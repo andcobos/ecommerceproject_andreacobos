4f682e715485b6e259a4bfbcce78b29105481aca
andcobos <146792784+andcobos@users.noreply.github.com>
2025-07-18 11:38:00 -0600

    chapter 3

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,12 +1,11 @@
-'user server';
-import { PrismaClient } from "@prisma/client";
+'use server';
+import { prisma } from "@/db/prisma";
 import { convertToPlainObject } from "../utils";
 import { LATEST_PRODUCTS_LIMIT } from "../constants";
 
 //function to fetch latest products to return prisma product and convert it to plain js obj
 // Get latest products
 export async function getLatestProducts(){
-    const prisma = new PrismaClient();
 
     const data = await prisma.product.findMany({
         take: LATEST_PRODUCTS_LIMIT,
@@ -15,4 +14,11 @@ export async function getLatestProducts(){
     });
 
     return convertToPlainObject(data);
+}
+
+//get single product by its slug
+export async function getProductBySlug(slug: string) {
+    return await prisma.product.findFirst({
+        where: { slug: slug },
+    });
 }
