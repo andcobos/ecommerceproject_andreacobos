9041e96b400231b7e67a14e78fce2de81dfaf5ff
andcobos <146792784+andcobos@users.noreply.github.com>
2025-07-21 23:26:59 -0600

    avance cap3

--- db/seed.ts
+++ db/seed.ts
@@ -3,11 +3,14 @@ import sampleData from "./sample-data";
 
 async function main() {
     const prisma = new PrismaClient();
-    //eliminar datos que ya estan en la base de datos
     await prisma.product.deleteMany();
+    await prisma.account.deleteMany();
+    await prisma.session.deleteMany();
+    await prisma.verificationToken.deleteMany();
+    await prisma.user.deleteMany();
 
-    //crear producto
     await prisma.product.createMany({data: sampleData.products});
+    await prisma.user.createMany({data: sampleData.users});
 
     console.log("Sample data seeded successfully.");
 }
--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -4,3 +4,8 @@ NEXT_PUBLIC_APP_NAME || 'Modern e-commerce Store built with Next.js';
 export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 
 'http://localhost:3000';
 export const LATEST_PRODUCTS_LIMIT = Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
+
+export const signInDefaultValues = {
+    email: '',
+    password: '',
+}
