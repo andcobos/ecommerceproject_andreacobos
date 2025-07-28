63ead41c81ff7d6ffabe7ba4508d7df9be474ec9
andcobos <146792784+andcobos@users.noreply.github.com>
2025-07-27 19:02:04 -0600

    ya sirve la vista del carrito

--- app/(root)/cart/cart-table.tsx
+++ app/(root)/cart/cart-table.tsx
@@ -3,6 +3,7 @@ import { useRouter } from 'next/navigation';
 import { useTransition } from 'react';
 import { addItemToCart, removeItemFromCart } from '@/lib/actions/cart.actions';
 import { ArrowRight, Loader, Minus, Plus } from 'lucide-react';
+import { toast } from 'sonner';
 import { Cart } from "@/types";
 import Link from 'next/link';
 import Image from 'next/image';
@@ -14,37 +15,110 @@ import {
   TableRow,
   TableCell,
 } from '@/components/ui/table';
-
+import { Button } from '@/components/ui/button';
 
 const CartTable = ({ cart }: { cart?: Cart }) => {
-    const router = useRouter();
-    const [isPending, startTransition] = useTransition()
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
 
-    return (
-        <>
-            <h1 className='py-4 h2-bold'>Shopping Cart</h1>
-            {!cart || cart.items.length === 0 ? (
-                <div>
-                    Cart is empty. <Link href='/'>Go Shopping</Link>
-                </div>
-                ) : (
-                  <div className='grid md:grid-cols-4 md:gap-5'>
-                    <div className='overflow-x-auto md:col-span-3'>
-                        <Table>
-                            <TableHeader>
-                                <TableRow>
-                                    <TableHead>Item</TableHead>
-                                    <TableHead className='text-center'>Quiantity</TableHead>
-                                    <TableHead className='text-right'>Price</TableHead>
-                                </TableRow>
-                            </TableHeader>
-                        </Table>
-                    </div>
-                  </div>  
-                ) }
+  return (
+    <>
+      <h1 className="py-4 h2-bold">Shopping Cart</h1>
+      {!cart || cart.items.length === 0 ? (
+        <div>
+          Cart is empty. <Link href="/">Go Shopping</Link>
+        </div>
+      ) : (
+        <div className="grid md:grid-cols-4 md:gap-5">
+          <div className="overflow-x-auto md:col-span-3">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Item</TableHead>
+                  <TableHead className="text-center">Quantity</TableHead>
+                  <TableHead className="text-right">Price</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {cart.items.map((item) => (
+                  <TableRow key={item.slug}>
+                    <TableCell>
+                      <Link
+                        href={`/product/${item.slug}`}
+                        className="flex items-center"
+                      >
+                        <Image
+                          src={item.image}
+                          alt={item.name}
+                          width={50}
+                          height={50}
+                        />
+                        <span className="px-2">{item.name}</span>
+                      </Link>
+                    </TableCell>
+                    <TableCell className="flex-center gap-2">
+                      {/* Remover producto */}
+                      <Button
+                        disabled={isPending}
+                        variant="outline"
+                        type="button"
+                        onClick={() =>
+                          startTransition(async () => {
+                            const res = await removeItemFromCart(item.productId);
+                            if (!res.success) {
+                              toast.error(res.message);
+                            } else {
+                              toast.success(`${item.name} removed from cart`);
+                              router.refresh(); // <- refresca UI
+                            }
+                          })
+                        }
+                      >
+                        {isPending ? (
+                          <Loader className="w-4 h-4 animate-spin" />
+                        ) : (
+                          <Minus className="w-4 h-4" />
+                        )}
+                      </Button>
 
-        </>
-    )
-}
- 
-export default CartTable; 
+                      <span>{item.qty}</span>
+
+                      {/* Agregar producto */}
+                      <Button
+                        disabled={isPending}
+                        variant="outline"
+                        type="button"
+                        onClick={() =>
+                          startTransition(async () => {
+                            const res = await addItemToCart(item);
+                            if (!res.success) {
+                              toast.error(res.message);
+                            } else {
+                              toast.success(`${item.name} added to cart`);
+                              router.refresh(); // <- refresca UI
+                            }
+                          })
+                        }
+                      >
+                        {isPending ? (
+                          <Loader className="w-4 h-4 animate-spin" />
+                        ) : (
+                          <Plus className="w-4 h-4" />
+                        )}
+                      </Button>
+                    </TableCell>
+                    <TableCell className="text-right">
+                      ${item.price}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default CartTable;
