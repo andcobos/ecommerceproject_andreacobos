df8214b7b0ee305523f4de5ad23203f2129e0e10
andcobos <146792784+andcobos@users.noreply.github.com>
2025-07-30 12:17:16 -0600

    chapter 12 doneeeee

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { shippingAddressSchema, signInFormSchema, signUpFormSchema, paymentMethodSchema} from '../validators';
+import { shippingAddressSchema, signInFormSchema, signUpFormSchema, paymentMethodSchema, updateUserSchema} from '../validators';
 import { auth, signIn, signOut } from '@/auth';
 import { hashSync } from 'bcrypt-ts-edge';
 import { isRedirectError } from 'next/dist/client/components/redirect-error';
@@ -194,5 +194,27 @@ export async function deleteUser(id: string) {
       success: false,
       message: formatError(error),
     };
+  }
+}
+
+// Update a user
+export async function updateUser(user: z.infer<typeof updateUserSchema>) {
+  try {
+    await prisma.user.update({
+      where: { id: user.id },
+      data: {
+        name: user.name,
+        role: user.role,
+      },
+    });
+
+    revalidatePath('/admin/users');
+
+    return {
+      success: true,
+      message: 'User updated successfully',
+    };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
   }
 }
